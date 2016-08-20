66d98288cec2546a50f6ad07d84239a55344b5d9
taylor <taylorsilva@outlook.com>
2016-08-19 22:49:54 -0400

    Original javascript code modified to meet site requirements set in unit 1.
    References to various sites I used to resolve various errors I was getting as I modified the code:
    
    Error I had with for loop and the FileReader(): http://stackoverflow.com/questions/24843508/filereader-error-the-object-is-already-busy-reading-blobs Was still getting the error, found idea to use recursive function so it automatically calls the next file when it’s done: http://stackoverflow.com/a/27555000/2081540
    
    Needed to use a queue in order to get the recursive function to work: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#Methods_2

--- javascript/postAdForm.js
+++ javascript/postAdForm.js
@@ -4,19 +4,71 @@
 */
 
 function readURL(input) {
+	// Ensure at least one image is selected and there's no more than 4
+    if (input.files[0] && (input.files.length <= 4)) {
+    	// counter to keep track of the element ID to be modified
+    	var i = 0; 
+    	// Queue containing all the images
+    	var images = new Array();
+    	// put each image into the queue
+    	for (var j = 0; j <= input.files.length - 1; j++) {
+    		images.push(input.files[j]);
+    	};
 
-    if (input.files[0]) {
-        var reader = new FileReader();
-        
-        reader.onload = function (e) {
-            document.getElementById("imgPreview").src = e.target.result;
-            document.getElementById("imgPreview").style = "display: block";
-        }
+    	var reader = new FileReader();
 
-        reader.readAsDataURL(input.files[0]);
-    }
-}
+    	// Function that will set the image(s) recursively
+    	function setImage() {
+    		var imgElementId = "imgPreview" + i;
+    		
+    		reader.onload = function (e) {
+        		console.log(imgElementId);
+        		var imgElement = document.getElementById(imgElementId);
+	            imgElement.src = e.target.result;
+	            imgElement.style = "display: block";
+
+	            // increment counter to get right element ID
+	            i++;
+
+	            // check if any images are still in the queue
+	            if (images.length > 0) {
+	            	// Images still left, set the next one
+	            	setImage();
+	            } else{
+	            	// no images left, exits function
+	            };
+        	};
+        	// Will set the url and remove it from the queue
+        	var imageData = images.shift();
+        	reader.readAsDataURL(imageData);
+    	};  
+
+    	// begin setting images
+    	setImage();	
+
+    } else {
+    	// Error handling. Most likely case is more than 4 images selected
+    	if (input.files.length > 4) {
+    		alert("You can only upload 4 photos");
+    	} else {
+    		// Unable to preview image(s) for other reason, fail silently
+    	};
+    };
+};
+
+function resetImageHtml () {
+	var imgElementId;
+
+	for (var i = 0; i <= 3; i++) {
+		imgElementId = "imgPreview" + i;
+		var imgElement = document.getElementById(imgElementId);
+		imgElement.src = "";
+		imgElement.style = "";
+	};
+};
 
 document.getElementById("photos").onchange = function(){
+	// reset HTML in case user has re-selected photos
+	resetImageHtml();
     readURL(this);
 };
