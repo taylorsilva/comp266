c4e02f4651f6c1d6f36e283ab0a5dc8753d1b99f
taylor <taylorsilva@outlook.com>
2016-08-20 02:23:31 -0400

    Completely decoupled all functions from the HTML, making them very reusable. Tested and works in Chrome, Firefox, Safari

--- javascript/postAdForm.js
+++ javascript/postAdForm.js
@@ -3,7 +3,7 @@
 	of the website.
 */
 
-function previewImages(input) {
+function previewImages(input, imgId, parentId, cssClass) {
 	// Ensure at least one image is selected and there's no more than 4
     if (input.files[0] && (input.files.length <= 4)) {
     	// counter to keep track of the element ID to be modified
@@ -17,15 +17,23 @@ function previewImages(input) {
 
     	var reader = new FileReader();
 
+    	// Create handle to parent element
+    	var parent = document.getElementById(parentId);
+
     	// Function that will set the image(s) recursively
     	function setImage() {
-    		var imgElementId = "imgPreview" + i;
+    		var imgElementId = imgId + i;
     		
     		reader.onload = function (e) {
-        		console.log(imgElementId);
-        		var imgElement = document.getElementById(imgElementId);
-	            imgElement.src = e.target.result;
-	            imgElement.style.display = "inline-block";
+        		console.log(imgElementId); // For debugging
+
+        		// Create image element and set attributes
+        		var imgElement = document.createElement("img");
+        		imgElement.src = e.target.result;
+        		imgElement.className = cssClass;
+        		imgElement.id = imgElementId; 
+        		// Append image element to the parent element
+        		parent.appendChild(imgElement);
 
 	            // increment counter to get right element ID
 	            i++;
@@ -56,19 +64,23 @@ function previewImages(input) {
     }
 }
 
-function resetImageHtml () {
-	var imgElementId;
+// Removes all elements with the matching ID + i number, starting at 0
+function removeElements (elementId) {
+	var i = 0; // counter to keep track of loop
+	// Get the first element
+	var imgElement = document.getElementById(elementId + i);
 
-	for (var i = 0; i <= 3; i++) {
-		imgElementId = "imgPreview" + i;
-		var imgElement = document.getElementById(imgElementId);
-		imgElement.src = "";
-		imgElement.style = "";
+	// loop until all elements are removed
+	while (imgElement) {
+		imgElement.remove();
+		// Get next element. Loop will end if no next element exists
+		i++
+		imgElement = document.getElementById(elementId + i);
 	}
 }
 
 document.getElementById("photos").onchange = function(){
 	// reset HTML in case user has re-selected photos
-	resetImageHtml();
-    previewImages(this);
+	removeElements("imgPreview");
+    previewImages(this, "imgPreview", "photoSection", "imagePreview");
 };
