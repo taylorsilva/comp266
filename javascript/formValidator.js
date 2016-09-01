e420f29eef6ee9d557cecbe8da0943996b3c358e
taylor <taylorsilva@outlook.com>
2016-08-31 22:35:08 -0400

    Reorganized the function structure to make it more class-like

--- javascript/formValidator.js
+++ javascript/formValidator.js
@@ -11,109 +11,96 @@ var TYPE_SCHOOL = 4;
 var TYPE_DEPARTMENT = 5;
 var TYPE_COURSECODE = 6;
 
-/*
-	Helper functions
-*/
-
-// Set class of the given element
-function setClass (element, className) {
-	// body...
-}
 
 /*
-	Individual input verification functions
-	Return boolean
-*/
-
-// Verify that a name has been entered
-function verifyFName (element) {
-	// body...
-}
-
-// verify that an email address has been provided
-function verifyEmail (element) {
-	// body...
-}
-
-// verify that a price has been entered
-function verifyPrice (element) {
-	// body...
-}
-
-// verify that text has been entered
-function verifyInputText (element) {
-	// body...
-}
-
-function verifySchool (element) {
-	// body...
-}
-
-function verifyDepartment (element) {
-	// body...
-}
-
-function verifyCourseCode (element) {
-	// body...
-}
-
-
-/*
-	Main listener function set to each input field in the form
+	Main listener function/class? set to each input field in the form
 */
 function fieldListener (event, inputType, errorClass, successClass) {
-	switch (inputType) {
-		case TYPE_FNAME:
-			if (verifyFName(event.target)) {
-				setClass(event.target, successClass);
-			} else {
-				setClass(event.target, errorClass);
-			};
-			break;
-		case TYPE_EMAIL:
-			if (verifyFName(event.target)) {
-				setClass(event.target, successClass);
-			} else {
-				setClass(event.target, errorClass);
-			};
-			break;
-		case TYPE_PRICE:
-			if (verifyFName(event.target)) {
-				setClass(event.target, successClass);
-			} else {
-				setClass(event.target, errorClass);
-			};
-			break;
-		case TYPE_TEXT:
-			if (verifyFName(event.target)) {
-				setClass(event.target, successClass);
-			} else {
-				setClass(event.target, errorClass);
-			};
-			break;
-		case TYPE_SCHOOL:
-			if (verifyFName(event.target)) {
-				setClass(event.target, successClass);
-			} else {
-				setClass(event.target, errorClass);
-			};
-			break;
-		case TYPE_DEPARTMENT:
-			if (verifyFName(event.target)) {
-				setClass(event.target, successClass);
-			} else {
-				setClass(event.target, errorClass);
-			};
-			break;
-		case TYPE_COURSECODE:
-			if (verifyFName(event)) {
-				setClass(event.target, successClass);
-			} else {
-				setClass(event.target, errorClass);
-			};
-			break;
-		default:
-			console.log("Unknow input type passed");
-			break;
+
+	/*
+		Private Helper functions
+	*/
+
+	// Set class of the given element
+	function setClass (element, className) {
+		element.className = className;
 	}
+
+	// evals a boolean function and sets its elements class accordingly 
+	function evalInput(inputValue) {
+		if (inputValue) {
+			setClass(event.target, successClass);
+		} else {
+			setClass(event.target, errorClass);
+		};
+	}
+
+	/*
+		Individual input verification functions
+		Return boolean
+	*/
+
+	// Verify that a name has been entered
+	function verifyFName (element) {
+		// body...
+	}
+
+	// verify that an email address has been provided
+	function verifyEmail (element) {
+		// body...
+	}
+
+	// verify that a price has been entered
+	function verifyPrice (element) {
+		// body...
+	}
+
+	// verify that text has been entered
+	function verifyInputText (element) {
+		// body...
+	}
+
+	function verifySchool (element) {
+		// body...
+	}
+
+	function verifyDepartment (element) {
+		// body...
+	}
+
+	function verifyCourseCode (element) {
+		// body...
+	}
+
+	// main function for this class?... javascript conventions are weird...
+	function main() {
+		switch (inputType) {
+			case TYPE_FNAME:
+				evalInput(verifyFName(event.target));
+				break;
+			case TYPE_EMAIL:
+				evalInput(verifyFName(event.target));
+				break;
+			case TYPE_PRICE:
+				evalInput(verifyFName(event.target));
+				break;
+			case TYPE_TEXT:
+				evalInput(verifyFName(event.target));
+				break;
+			case TYPE_SCHOOL:
+				evalInput(verifyFName(event.target));
+				break;
+			case TYPE_DEPARTMENT:
+				evalInput(verifyFName(event.target));
+				break;
+			case TYPE_COURSECODE:
+				evalInput(verifyFName(event));				
+				break;
+			default:
+				console.log("Unknow input type passed");
+				break;
+		}
+	}
+
+	main();
 }
