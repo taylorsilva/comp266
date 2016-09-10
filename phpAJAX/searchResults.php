4e2126920ca32db815d91cd35e50fdc13435da68
taylor <taylorsilva@outlook.com>
2016-09-10 18:31:50 -0400

    Added files used in creating the search results area on the homepage

--- phpAJAX/searchResults.php
+++ phpAJAX/searchResults.php
@@ -1,5 +1,19 @@
 <?php
 // This is all dummy data just to proof-of-concept my textbook website
 
- 
+
+include 'textbooks.php'; // includes master list of fake textbook listings
+
+// create random list of textbook listings
+$ranTextbookKeys = array_rand($masterTextbookList, 10);
+
+// Create new array from the 10 randomly selected keys
+$textbooks = array();
+foreach ($ranTextbookKeys as $key => $value) {
+	// $value contains the textbook ID
+	$textbooks[$value] = $masterTextbookList[$value];
+}
+
+print(json_encode($textbooks));
+
 ?>
