19470cd8ccc36d0f46cf44d116f1ae9e7dfd814e
taylor <taylorsilva@outlook.com>
2016-09-10 21:53:28 -0400

    Added loading spinner when search form is submitted.
    Source: https://codepen.io/jczimm/pen/vEBpoL

--- javascript/searchResults.js
+++ javascript/searchResults.js
@@ -3,3 +3,18 @@
 	Requires jQuery
 */
 
+// Loading spinner HTML source: https://codepen.io/jczimm/pen/vEBpoL
+var loadingSpinner = '<div class="showbox">\
+  <div class="loader">\
+    <svg class="circular" viewBox="25 25 50 50">\
+      <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="3" stroke-miterlimit="10"/>\
+    </svg>\
+  </div>\
+  <div>Finding Textbooks</div>\
+</div>';
+
+//Bind to submit event for the search form
+$("#searchForm").submit(function (event) {
+	// Delete featured post and replace with loading wheel
+	$("#featuredPost").replaceWith(loadingSpinner);
+});
