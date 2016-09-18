2628e4f73768b908abfe25dd3a5e6888812eb058
taylor <taylorsilva@outlook.com>
2016-09-18 00:15:15 -0400

    Made courseSelector.js more modular by putting everything into their own functions.

--- javascript/courseSelector.js
+++ javascript/courseSelector.js
@@ -8,9 +8,7 @@ var ajax = new XMLHttpRequest()
 				,method = "GET"
 				,url = "http://student.athabascau.ca/~taylorsi5/phpAJAX/searchSelector.php";
 
-// Load schools into <select> as soon as document is loaded
-$(document).ready(function () {
-
+function loadSchools () {
 	// called once response is returned
 	ajax.onreadystatechange = function () {
 		if (ajax.status == 200 && ajax.readyState == 4) { 
@@ -27,11 +25,10 @@ $(document).ready(function () {
 
 	ajax.open(method, url, true);
 	ajax.send();
-});
+}
 
-// Load departments based on the selected school
-$(document).on('change', '#school', function (e) {
-	// load courses
+function loadDepartments () {
+
 	ajax.onreadystatechange = function () {
 		if (ajax.status == 200 && ajax.readyState == 4) {
 			var courses = JSON.parse(ajax.responseText);
@@ -49,19 +46,18 @@ $(document).on('change', '#school', function (e) {
 
 	ajax.open(method, url + '?school=' + $('#school').val(), true);
 	ajax.send();
-});
+}
 
-// Load course code based on the selected school
-$(document).on('change', '#department', function (e) {
-	// load courses
+function loadCourseCodes () {
+
 	ajax.onreadystatechange = function () {
 		if (ajax.status == 200 && ajax.readyState == 4) {
 			var coursecodes = JSON.parse(ajax.responseText)[$('#department').val()];
 			//clear out current options for department
-			$('#coursecode').empty();
+			$('#courseCode').empty();
 			// Insert new options for department
 			$.each(coursecodes, function (key, value) {
-				$('#coursecode').append($('<option>', {
+				$('#courseCode').append($('<option>', {
 					value: value,
 					text: value
 				}));
@@ -71,4 +67,19 @@ $(document).on('change', '#department', function (e) {
 
 	ajax.open(method, url + '?school=' + $('#school').val(), true);
 	ajax.send();
+}
+
+// Load schools into <select> as soon as document is loaded
+$(document).ready(function () {
+	loadSchools();
+});
+
+// Load departments based on the selected school
+$(document).on('change', '#school', function (e) {
+	loadDepartments();
+});
+
+// Load course code based on the selected school
+$(document).on('change', '#department', function (e) {
+	loadCourseCodes();
 });
